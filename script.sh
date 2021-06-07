f39bc533d8256dc05e66b493e31bb37680b1cd3e
bettaieb <medali.bettaieb@esprit.tn>
2021-06-07 14:58:59 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,6 +16,10 @@ sudo sh test-docker.sh
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+# Install Portainer
+docker volume create portainer_data
+docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent
+
 # Start stacks
 git clone https://github.com/medalibettaieb/goat-wolf.git
 cd goat-wolf
